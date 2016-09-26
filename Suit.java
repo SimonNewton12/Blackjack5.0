0a66d1b5ee80f710a0b5175c71b4f231a85ca6c0
akosborn <andrewosborn93@gmail.com>
2016-09-26 00:09:44 -0500

    Added constructor to enum Suit

--- Suit.java
+++ Suit.java
@@ -2,8 +2,15 @@ package osborn.andrew.blackjack;
 
 public enum Suit
 {
-    SPADE,
-    HEART,
-    CLUB,
-    DIAMOND;
+    SPADE("Spade"),
+    HEART("Heart"),
+    CLUB("Club"),
+    DIAMOND("Diamond");
+
+    private String suit;
+
+    Suit(String suit)
+    {
+        this.suit = suit;
+    }
 }
