737bc231b21be60626c173ee0d920d74fbe8a760
akosborn <andrewosborn93@gmail.com>
2016-09-27 17:17:41 -0500

    Added deal(), hit(), and static variables HIT and DEAL

--- Blackjack.java
+++ Blackjack.java
@@ -1,24 +1,36 @@
 package osborn.andrew.blackjack;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Blackjack
 {
-    public static void main(String[] args)
+    Deck playingDeck;
+    private static int DEAL = 2;
+    private static int HIT = 1;
+
+    public Blackjack()
     {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Let's play blackjack!");
-        Deck playingDeck = new Deck();
+        playingDeck = new Deck();
         System.out.print("How many decks would you like to play with?");
-        int numDecks = input.nextInt();
 
         // amount of decks to be played with is set by user and sent as argument
+        int numDecks = input.nextInt();
         playingDeck.createFullDeck(numDecks);
         playingDeck.shuffle();
-        Card dealtCard = playingDeck.dealCard();
-        System.out.println(dealtCard);
+    }
+
+    public void deal()
+    {
+        String dealtCards = playingDeck.dealCard(DEAL);
+        System.out.println(dealtCards);
+    }
+
+    public void hit()
+    {
+        String dealtCards = playingDeck.dealCard(HIT);
+        System.out.println(dealtCards);
     }
 }
