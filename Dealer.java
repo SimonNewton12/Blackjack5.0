e669f6e274209f9478fefdf44eeed3d16be4c28e
akosborn <andrewosborn93@gmail.com>
2016-09-27 23:20:45 -0500

    Added addCards() and getCards()

--- Dealer.java
+++ Dealer.java
@@ -2,4 +2,15 @@ package osborn.andrew.blackjack;
 
 public class Dealer
 {
+    private Card cards;
+
+    public void addCards()
+    {
+
+    }
+
+    public Card getCards()
+    {
+        return cards;
+    }
 }
