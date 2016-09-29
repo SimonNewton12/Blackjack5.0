5b87b50c2cfde0e2632c30e73ed4888e5a8e59cb
akosborn <andrewosborn93@gmail.com>
2016-09-29 00:56:03 -0500

    Added createPlayers()

--- Blackjack.java
+++ Blackjack.java
@@ -7,6 +7,7 @@ public class Blackjack
     private Database database;
     private Dealer dealer;
     private Deck playingDeck;
+    private int numPlayers;
     private Scanner input = new Scanner(System.in);
     private static int DEAL = 2;
     private static int HIT = 1;
@@ -16,34 +17,43 @@ public class Blackjack
         database = new Database();
         dealer = new Dealer();
 
-        System.out.println("Let's play blackjack!");
+        System.out.println("Let's play blackjack!\n");
+        System.out.print("How many players? ");
+        numPlayers = input.nextInt();
+        createPlayers();
+
         playingDeck = new Deck();
-        System.out.print("How many decks would you like to play with?");
+        System.out.print("How many decks would you like to play with? ");
 
         // amount of decks to be played with is set by user and sent as argument
         int numDecks = input.nextInt();
         playingDeck.createFullDeck(numDecks);
         playingDeck.shuffle();
-
-        createPlayers();
     }
 
-    public void createPlayers()
+    /**
+     * createPlayers(int numPlayers) creates the appropriate amount of players
+     */
+    private void createPlayers()
     {
-        System.out.print("Player 1, enter your name: ");
-        String name = input.next();
-        System.out.println();
-        int bankroll = input.nextInt();
-        new Player(name, bankroll);
+        for (int i = 1; i < numPlayers + 1; i++)
+        {
+            System.out.print("Player " + i + "'s name: ");
+            String name = input.next();
+            System.out.print("Player " + i + "'s deposit: ");
+            int bankroll = input.nextInt();
+            new Player(name, bankroll);
+            System.out.println("");
+        }
     }
 
-    public void deal()
+    private void deal()
     {
         String dealtCards = playingDeck.dealCard(DEAL);
         System.out.println(dealtCards);
     }
 
-    public void hit()
+    private void hit()
     {
         String dealtCards = playingDeck.dealCard(HIT);
         System.out.println(dealtCards);
